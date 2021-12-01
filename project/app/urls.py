b00d57206fbfe0c962d798c3d3c42f8ed8a0540f
Lucas Hollas de Cairos <acclucash@gmail.com>
2021-12-01 15:55:56 -0300

    TaskView update

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list, name='list'),
+    path('task/<int:id>', views.taskView, name="task-view"),
     path('yourname/<str:name>', views.yourName, name="your-name")
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Task
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'app/task.html', {'task': task})
+
 def list(request):
-    return render(request, 'app/list.html')
+    tasks = Task.objects.all()
+    return render(request, 'app/list.html', {'tasks': tasks})
 
 def yourName(request, name):
     return render(request, 'app/yourname.html', {'name': name })
