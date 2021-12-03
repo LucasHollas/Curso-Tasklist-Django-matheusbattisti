f883e8717a3019ea36bf5f077e4435c0925018c2
lucashollas <acclucash@gmail.com>
2021-12-03 16:58:34 -0300

    Login and django auth implementation

--- project/app/views.py
+++ project/app/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import redirect, render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 from .models import Task
 from .forms import TaskForm
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'app/task.html', {'task': task})
 
+@login_required
 def taskList(request):
 
     search = request.GET.get('search')
@@ -31,6 +34,7 @@ def taskList(request):
 
     return render(request, 'app/list.html', {'tasks': tasks})
 
+@login_required
 def newTask(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -45,6 +49,7 @@ def newTask(request):
         form = TaskForm()
         return render(request, 'app/addtask.html', {'form': form})
 
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -60,6 +65,7 @@ def editTask(request, id):
     else:
         return render(request, 'app/edittask.html', {'form': form, 'task': task})
 
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,7 +61,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +127,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
