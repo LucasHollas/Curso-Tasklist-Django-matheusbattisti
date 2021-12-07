10bc9e322a239a622e1c4c6cf8d498f73883d704
lucashollas <acclucash@gmail.com>
2021-12-07 16:48:34 -0300

    filter for done or doing

--- project/app/views.py
+++ project/app/views.py
@@ -17,10 +17,15 @@ def taskView(request, id):
 def taskList(request):
 
     search = request.GET.get('search')
+    filter = request.GET.get('filter')
 
     if search:
 
         tasks = Task.objects.filter(title__icontains=search, user = request.user)
+
+    elif filter:
+
+        tasks = Task.objects.filter(done=filter, user = request.user)
 
     else:
 
